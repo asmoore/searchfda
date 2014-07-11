8c7f54d4e1a2b6e4ce6a31179c2bf6b1b90f2284
asmoore <asmoor@gmail.com>
2014-07-11 09:21:32 -0400

    Update tasks.py
    Started index_search using whoosh

--- tasks.py
+++ tasks.py
@@ -8,3 +8,22 @@ tasks
 Tasks functions for the `searchfda` module.
 
 """
+
+from whoosh.index import create_in
+from whoosh.fields import *
+
+index_search():
+    """
+    Index OpdenFDA for searching.
+
+
+    """
+
+    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+    ix = create_in("indexdir", schema)
+    writer = ix.writer()
+    writer.add_document(title=u"First document", path=u"/a", content=u"This is the first document we've added!")
+    writer.add_document(title=u"Second document", path=u"/b",
+    content=u"The second one is even more interesting!")
+    writer.commit()
+
