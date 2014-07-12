965d62ced1b81c07de251ab953356dc9c656e97c
Andrew Moore <asmoor@gmail.com>
2014-07-12 08:44:02 -0400

    added folder for index_search data

--- tasks.py
+++ tasks.py
@@ -9,21 +9,25 @@ Tasks functions for the `searchfda` module.
 
 """
 
+import os
+
 from whoosh.index import create_in
 from whoosh.fields import *
 
-index_search():
+def index_search():
     """
     Index OpdenFDA for searching.
 
 
     """
-
+    root = test = os.path.dirname(os.path.realpath('__file__'))
     schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
-    ix = create_in("indexdir", schema)
+    ix = create_in(root+"/data/", schema)
     writer = ix.writer()
     writer.add_document(title=u"First document", path=u"/a", content=u"This is the first document we've added!")
-    writer.add_document(title=u"Second document", path=u"/b",
-    content=u"The second one is even more interesting!")
+    writer.add_document(title=u"Second document", path=u"/b",content=u"The second one is even more interesting!")
     writer.commit()
 
+
+if __name__ == '__main__':
+    index_search()
