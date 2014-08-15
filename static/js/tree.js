7967814a638327ebd8aafa3907d7646102338a6e
Andrew Moore <asmoor@gmail.com>
2014-08-14 20:04:08 -0400

    added values to adverseevents

--- static/js/tree.js
+++ static/js/tree.js
@@ -5,14 +5,38 @@ function makeAdverseEvent(adverseEvent,chartName) {
 function visitObj($container, jsonData) {
     var $ul = $('<ul class="report">');
     
+    var $li = $('<li>')
+    $li.append('<h5>General Information</h5>');
+    $ul.append($li);
+
     var $li = $('<li>');
     receiveDate = jsonData['receivedate'];
     $li.append('<span class="json-key">Date: </span>');
     $li.append('<span class="date">' + receiveDate + '</span>');
     $ul.append($li);
+
+    var $li = $('<li>');
+    occurCountry = jsonData['occurcountry'];
+    $li.append('<span class="json-key">Country of occurence: </span>');
+    $li.append('<span class="date">' + occurCountry + '</span>');
+    $ul.append($li);
+
+    var $li = $('<li>');
+    serious = jsonData['serious'];
+    if (serious=="1"){
+        $li.append('<span class="json-key">Serious: </span>');
+        $li.append('<span class="date">' + 'The adverse event resulted in death, a life threatening condition, hospitalization, disability, congenital anomali, or other serious condition.' + '</span>');
+        $ul.append($li);
+    }
+    else {
+        $li.append('<span class="json-key">Country of occurence: </span>');
+        $li.append('<span class="date">' + 'Not serious' + '</span>');
+        $ul.append($li);
+    }
+    
     
      var $li = $('<li>')
-    $li.append('<h2>Reactions</h2>');
+    $li.append('<h5>Reactions</h5>');
     $ul.append($li);
     
     for (var i=0; i<jsonData['patient']['reaction'].length; i++) {
@@ -25,11 +49,17 @@ function visitObj($container, jsonData) {
     }
     
     var $li = $('<li>')
-    $li.append('<h2>Drugs used</h2>');
+    $li.append('<h5>Drugs used</h5>');
     $ul.append($li);
     
     for (var i=0; i<jsonData['patient']['drug'].length; i++) {
         
+        var $li = $('<li>')
+        dosage = jsonData['patient']['drug'][i]['drugdoseagetext'];
+        $li.append('<span class="json-key">Dosage: </span>');
+        $li.append('<span class="json-value">' + dosage + '</span>')
+        $ul.append($li);
+
         var $li = $('<li>')
         indication = jsonData['patient']['drug'][i]['drugindication'];
         $li.append('<span class="json-key">Indication: </span>');
